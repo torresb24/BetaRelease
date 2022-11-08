61ac379d0869eab82ca4dbab48602b580c2c55de
torresb24 <torresb24@up.edu>
2022-11-08 14:15:29 -0800

    localGame commit

---
+++ app/src/main/java/com/example/alpharelease/localGame.java
@@ -0,0 +1,24 @@
+package com.example.alpharelease;
+
+public class localGame {
+    /*Includes methods for everything:
+      selectPiece()
+      move[Piece Name]()
+      boundsCheck()
+      obstacles()
+      makeMove()
+      makeMove()
+      knightMove()
+
+
+      capturePiece()
+
+      FOR BETA:
+      promotePiece()
+      dropPiece()
+
+    */
+
+
+
+}
