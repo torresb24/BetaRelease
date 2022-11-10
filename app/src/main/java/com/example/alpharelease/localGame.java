7d0a524abd68711d80ff67481741bdb126d4b97e
torresb24 <torresb24@up.edu>
2022-11-10 14:00:14 -0800

    Update localGame.java
    move[Piece]()

--- app/src/main/java/com/example/alpharelease/localGame.java
+++ app/src/main/java/com/example/alpharelease/localGame.java
@@ -7,7 +7,6 @@ public class localGame {
       boundsCheck()
       obstacles()
       makeMove()
-      makeMove()
       knightMove()
 
 
@@ -18,6 +17,67 @@ public class localGame {
       dropPiece()
 
     */
+
+
+    public void moveKing(boolean turn, int x, int y) {
+           makeMove(turn, x, y, 1, 1, 1, 1, 1, 1, 1, 1);
+    }
+
+
+    public void moveGoldGen(boolean turn, int x, int y){
+        makeMove(turn, x, y, 1, 1, 1, 1, 1, 0, 1, 0);
+    }
+
+    public void moveSilvGen(boolean turn, int x, int y){
+        makeMove(turn, x, y, 1, 1, 1, 0, 0, 1, 0, 1);
+
+    }
+
+    public void movePromSilvGen(boolean turn, int x, int y){
+        makeMove(turn, x, y, 1, 1, 1, 1, 1, 0, 1, 0);
+    }
+
+    public void moveBishop(boolean turn, int x, int y){
+        makeMove(turn, x, y, 1, 1, 1, 1, 1, 1, 1, 1);
+    }
+
+    public void movePromBishop(boolean turn, int x, int y){
+
+    }
+
+    public void moveRook(boolean turn, int x, int y){
+
+    }
+
+    public void movePromRook(boolean turn, int x, int y){
+
+    }
+
+    public void moveLance(boolean turn, int x, int y){
+
+    }
+
+    public void movePromLance(boolean turn, int x, int y){
+
+    }
+
+    public void moveKnight(boolean turn, int x, int y){
+
+    }
+
+    public void movePromKnight(boolean turn, int x, int y){
+
+    }
+
+    public void movePawn(boolean turn, int x, int y){
+
+    }
+
+    public void movePromPawn(boolean turn, int x, int y){
+
+    }
+
+
 
 
 
