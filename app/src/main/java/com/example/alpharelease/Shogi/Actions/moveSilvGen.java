a85de778c024a39258a64d2c856c2a1c2df8d6f8
Aurora <kathryn.aurora.w@gmail.com>
2022-11-18 14:54:34 -0800

    Making the game actions

---
+++ app/src/main/java/com/example/alpharelease/Shogi/Actions/moveSilvGen.java
@@ -0,0 +1,15 @@
+package com.example.alpharelease.Shogi.Actions;
+
+import com.example.shogialpha.GameFramework.actionMessage.GameAction;
+import com.example.shogialpha.GameFramework.players.GamePlayer;
+
+public class moveSilvGen extends GameAction {
+    /**
+     * constructor for GameAction
+     *
+     * @param player the player who created the action
+     */
+    public moveSilvGen(GamePlayer player) {
+        super(player);
+    }
+}
