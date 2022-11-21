5e4ae05aa5d91bd7acd8fd195b3d622ad0a1544c
Aurora <kathryn.aurora.w@gmail.com>
2022-11-20 18:00:27 -0800

    Deleted promo gold gen pngs and the NewGameAction class. Fixed SelectPieceAction class and added MovePieceAction class

---
+++ app/src/main/java/com/example/alpharelease/Shogi/Actions/MovePieceAction.java
@@ -0,0 +1,19 @@
+package com.example.alpharelease.Shogi.Actions;
+
+import com.example.alpharelease.GameFramework.actionMessage.GameAction;
+import com.example.alpharelease.GameFramework.players.GamePlayer;
+
+public class MovePieceAction extends GameAction {
+
+    public int destination;
+
+    /**
+     * constructor for GameAction
+     *
+     * @param player the player who created the action
+     */
+    public MovePieceAction(GamePlayer player, int destinationIndex) {
+        super(player);
+        destination = destinationIndex;
+    }
+}
