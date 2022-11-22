db78e19d80f850bd01e1e43878455de7d254fb29
Aurora <kathryn.aurora.w@gmail.com>
2022-11-22 13:19:04 -0800

    Deleted extraneous classes (again???)

---
+++ app/src/main/java/com/example/alpharelease/Shogi/Actions/movePromBishop.java
@@ -0,0 +1,15 @@
+package com.example.alpharelease.Shogi.Actions;
+
+import com.example.alpharelease.GameFramework.actionMessage.GameAction;
+import com.example.alpharelease.GameFramework.players.GamePlayer;
+
+public class movePromBishop extends GameAction {
+    /**
+     * constructor for GameAction
+     *
+     * @param player the player who created the action
+     */
+    public movePromBishop(GamePlayer player) {
+        super(player);
+    }
+}
