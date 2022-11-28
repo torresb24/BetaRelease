4283cecb01cf49d5beb3c1914da54af80bd95914
kellye24 <103087185+kellye24@users.noreply.github.com>
2022-11-27 21:23:12 -0800

    simple test

--- app/src/test/java/com/example/alpharelease/ExampleUnitTest.java
+++ app/src/test/java/com/example/alpharelease/ExampleUnitTest.java
@@ -4,6 +4,9 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+import com.example.alpharelease.Shogi.ShogiLocalGame;
+import com.example.alpharelease.Shogi.Tile;
+
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
@@ -14,4 +17,41 @@ public class ExampleUnitTest {
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
     }
+
+    public void createTile_isCorrect() {
+        Tile t1 = new Tile();
+
+        t1.setCol(1);
+        t1.setRow(2);
+
+        assertEquals(1, t1.getCol());
+        assertEquals(2, t1.getRow());
+    }
+
+    public void setCords_isCorrect() {
+        Tile t1 = new Tile();
+
+        t1.setCords(1,2,3,4);
+
+        assertEquals(1,t1.getxCord());
+        assertEquals(2,t1.getyCord());
+        assertEquals(3,t1.getxCordEnd());
+        assertEquals(4,t1.getyCordEnd());
+    }
+
+    public void gameCopy_isCorrect() {
+        ShogiLocalGame g1 = new ShogiLocalGame();
+        ShogiLocalGame g2 = new ShogiLocalGame();
+
+        g2.moveKnight(false,1,1);
+
+        assertNotEquals(g1, g2);
+    }
+
+    /*public void setCords(int x, int y, int xEnd, int yEnd) {
+        this.xCord = x;
+        this.yCord = y;
+        this.xCordEnd = xEnd;
+        this.yCordEnd = yEnd;
+    }*/
 }
