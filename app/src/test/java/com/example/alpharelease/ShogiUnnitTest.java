a1730ec086f8f7af70d31f5850d357fd7437c9ee
Matttran7 <matttran385701@gmail.com>
2022-11-29 17:22:05 -0800

    3 unitTests + Gradle ReturnTrue

--- app/src/test/java/com/example/alpharelease/ShogiUnnitTest.java
+++ app/src/test/java/com/example/alpharelease/ShogiUnnitTest.java
@@ -9,15 +9,37 @@ import com.example.alpharelease.Shogi.ShogiGameState;
 
 public class ShogiUnnitTest {
     @Test
-    public void TestInitPiecces() throws Exception{
+    public void TestInitPieces() throws Exception{
         ShogiGameState testState = new ShogiGameState();
-        testState
-    }
+        int KingRow = -1;
+        int KingCol = -1;
+        for(int i = 0; i < testState.getPieceArray(0).size(); i++){
+            if(testState.getPieceArray(0).get(i).pieceType.getID() == R.drawable.king){
+                KingCol = testState.getPieceArray(0).get(i).getCol();
+                KingRow = testState.getPieceArray(0).get(i).getRow();
+                break;
+            }
+        }// for
+        assertEquals(4,KingCol);
+        assertEquals(0,KingRow);
+    } /** Matt Tran */
+
     @Test
     public void TestCopyConstructor() throws Exception {
         ShogiGameState testState = new ShogiGameState();
         testState.changeTurn(10);
         ShogiGameState testStateCopy = new ShogiGameState(testState);
         assertEquals(10,testStateCopy.getWhoseTurn());
-    }
+    } /** Matt Tran */
+
+    @Test
+    public void TestSetSelecting() throws Exception {
+        ShogiGameState testState = new ShogiGameState();
+        testState.setSelecting(true);
+        boolean t1 = testState.isSelecting();
+        testState.setSelecting(false);
+        boolean t2 = testState.isSelecting();
+        assertEquals(true,t1);
+        assertEquals(false,t2);
+    } /** Matt Tran */ 
 }
